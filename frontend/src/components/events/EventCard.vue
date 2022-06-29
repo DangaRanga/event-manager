d4b7566b82e808f55575df174a63eaf528a1ae74
Jason Gayle <jasonblue1626@gmail.com>
2022-06-29 13:02:57 -0500

    Completed events page layout without search

--- frontend/src/components/events/EventCard.vue
+++ frontend/src/components/events/EventCard.vue
@@ -1,10 +1,10 @@
 <template>
-  <article class="my-4 cursor-pointer">
+  <article class="my-4 cursor-pointer hover:shadow-md w-2/3 p-3 py-3">
     <div id="image" class="mb-2">
       <img src="" alt="event image" />
     </div>
     <div id="content">
-      <h2 class="text-lg w-2/3 font-bold">{{ title }}</h2>
+      <h2 class="text-lg font-bold">{{ title }}</h2>
       <h3 class="text-gray-700">{{ venue }}</h3>
       <p class="text-primary text-sm">{{ date }}</p>
     </div>
