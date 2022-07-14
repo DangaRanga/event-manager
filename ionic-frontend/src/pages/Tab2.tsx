3cef5491fb2093ef196bedf8b7c9988de47b200c
David Griffiths <griffithsdavidchris@gmail.com>
2022-07-13 22:28:30 -0500

    .

--- ionic-frontend/src/pages/Tab2.tsx
+++ ionic-frontend/src/pages/Tab2.tsx
@@ -1,20 +1,46 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import {
+	IonContent,
+	IonHeader,
+	IonPage,
+	IonTitle,
+	IonToolbar,
+} from '@ionic/react';
 import './Tab2.css';
+import EventForm from '../components/EventForm';
 
 const Tab2: React.FC = () => {
-  return (
-    <IonPage>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
-      </IonContent>
-    </IonPage>
-  );
+
+	const onSubmit= async(values:any, actions:any) => {
+		try{
+		
+			const config = {
+        'Authorization': ''
+			}
+    //  const result = await axios.post('localhoat:8080/api/v1/events', values, config);
+
+	    console.log(values)
+		}catch(error: any){
+
+		}
+
+	}
+	return (
+		<IonPage>
+			<IonHeader>
+				<IonToolbar>
+					<IonTitle>Add Event</IonTitle>
+				</IonToolbar>
+			</IonHeader>
+			<IonContent fullscreen>
+				<IonHeader collapse="condense">
+					<IonToolbar>
+						<IonTitle size="large">Add Event</IonTitle>
+					</IonToolbar>
+				</IonHeader>
+				<EventForm onSubmit={onSubmit} />
+			</IonContent>
+		</IonPage>
+	);
 };
 
 export default Tab2;
